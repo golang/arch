b19d1a73c7d6d5f0aa59f060561421b7ccdaedaf
Ben Shi <powerman1st@163.com>
2017-07-10 07:27:41 +0000

    arm/armasm: fix wrong register order in SMLABT/SMLATB/SMLATT/SMLAD/SMLSD
    SMLABT/SMLATB/SMLATT/SMLAD/SMLSD are MLA(mul&add) like instructions,
    which need the third register to be the addend. But currently the addend
    is placed in the first position in the decoded register list.
    
    This patch fixes it and adds corresponding tests.
    
    Change-Id: I57f3e96876c54497badf1a12ae7dae18fe6fcc11
    Reviewed-on: https://go-review.googlesource.com/47890
    Run-TryBot: Cherry Zhang <cherryyz@google.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Cherry Zhang <cherryyz@google.com>

--- arm/armasm/plan9x.go
+++ arm/armasm/plan9x.go
@@ -115,7 +115,7 @@ func GoSyntax(inst Inst, pc uint64, symname func(uint64) (string, uint64), text 
 	}
 	// For MLA-like instructions, the addend is the third operand.
 	switch inst.Op &^ 15 {
-	case SMLAWT_EQ, SMLAWB_EQ, MLA_EQ, MLA_S_EQ, MLS_EQ, SMMLA_EQ, SMMLS_EQ, SMLABB_EQ:
+	case SMLAWT_EQ, SMLAWB_EQ, MLA_EQ, MLA_S_EQ, MLS_EQ, SMMLA_EQ, SMMLS_EQ, SMLABB_EQ, SMLATB_EQ, SMLABT_EQ, SMLATT_EQ, SMLAD_EQ, SMLAD_X_EQ, SMLSD_EQ, SMLSD_X_EQ:
 		args = []string{args[1], args[2], args[0], args[3]}
 	}
 
